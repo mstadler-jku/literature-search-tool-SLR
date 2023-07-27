eb31253149ed77e88ba6cdb8f4b89284272e0cc3
Marco Stadler <marco.stadler@jku.at>
2023-07-27 13:44:28 +0200

    performed IEEE search string creation
    Signed-off-by: Marco Stadler <marco.stadler@jku.at>

---
+++ src/main/java/at/jku/scsl/main/IEEESearch.java
@@ -0,0 +1,210 @@
+package at.jku.scsl.main;
+
+import at.jku.scsl.searchtool.BaseQuery;
+import at.jku.scsl.searchtool.SearchTool;
+import at.jku.scsl.searchtool.types.BooleanOperatorType;
+import at.jku.scsl.searchtool.types.DatabaseType;
+import at.jku.scsl.searchtool.types.MetaDataType;
+import at.jku.scsl.util.Util;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.util.List;
+
+import static at.jku.scsl.util.Util.FILE_PATH_SEP;
+
+public class IEEESearch {
+
+    private static final Logger logger = LogManager.getLogger(IEEESearch.class);
+
+
+    private IEEESearch() {
+        throw new IllegalStateException("Utility class");
+    }
+
+    public static void searchIEEE() {
+        // #1
+//        searchIEEE_ONE();
+        // #2
+//        searchIEEE_TWO();
+        // #3
+//        searchIEEE_THREE();
+        // #4
+//        searchIEEE_FOUR();
+    }
+
+
+    private static void searchIEEE_FOUR() {
+        String keywordFilePathFirstHalf = "src" + FILE_PATH_SEP + "main" + FILE_PATH_SEP + "resources" + FILE_PATH_SEP + "keywords" + FILE_PATH_SEP + "keywords_v2_01.txt";
+        String keywordFilePathSecondHalf = "src" + FILE_PATH_SEP + "main" + FILE_PATH_SEP + "resources" + FILE_PATH_SEP + "keywords" + FILE_PATH_SEP + "keywords_v2_02.txt";
+        List<String> firstHalfKeywords = Util.readKeywords(keywordFilePathFirstHalf);
+        List<String> secHalfKeywords = Util.readKeywords(keywordFilePathSecondHalf);
+
+        logger.info("Found {} keywords in '{}' and {}", firstHalfKeywords.size() + secHalfKeywords.size(), keywordFilePathFirstHalf, keywordFilePathSecondHalf);
+
+        BaseQuery keywordQueryFirstHalf = new BaseQuery();
+        keywordQueryFirstHalf.setKeywordList(firstHalfKeywords);
+        keywordQueryFirstHalf.setBooleanOperatorType(BooleanOperatorType.OR);
+
+        BaseQuery keywordQuerySecondHalf = new BaseQuery();
+        keywordQuerySecondHalf.setKeywordList(secHalfKeywords);
+        keywordQuerySecondHalf.setBooleanOperatorType(BooleanOperatorType.OR);
+
+        BaseQuery softwareQuery = new BaseQuery();
+        softwareQuery.addKeyword("\"software\"");
+        softwareQuery.addKeyword("\"system\"");
+        softwareQuery.setBooleanOperatorType(BooleanOperatorType.OR);
+
+        BaseQuery securityQuery = new BaseQuery();
+        securityQuery.addKeyword("\"security\"");
+        securityQuery.setBooleanOperatorType(BooleanOperatorType.OR);
+
+
+        SearchTool searchtool = new SearchTool();
+        searchtool.addMetaDataFields(MetaDataType.TITLE, MetaDataType.ABSTRACT, MetaDataType.KEYWORDS);
+        searchtool.addBaseQueries(keywordQueryFirstHalf, softwareQuery, securityQuery);
+        searchtool.setBooleanOperatorType(BooleanOperatorType.AND);
+
+        String ieeeSearchStringFirstHalf = searchtool.generateSearchQuery(DatabaseType.IEEE_XPLORE);
+        logger.info("Created search String for IEEE 1: {}", ieeeSearchStringFirstHalf);
+
+        searchtool.reset();
+        searchtool.addMetaDataFields(MetaDataType.TITLE, MetaDataType.ABSTRACT, MetaDataType.KEYWORDS);
+        searchtool.addBaseQueries(keywordQuerySecondHalf, softwareQuery, securityQuery);
+        searchtool.setBooleanOperatorType(BooleanOperatorType.AND);
+
+        String ieeeSearchStringSecondHalf = searchtool.generateSearchQuery(DatabaseType.IEEE_XPLORE);
+        logger.info("Created search String for IEEE 2: {}", ieeeSearchStringSecondHalf);
+    }
+
+
+    private static void searchIEEE_THREE() {
+        String keywordFilePathFirstHalf = "src" + FILE_PATH_SEP + "main" + FILE_PATH_SEP + "resources" + FILE_PATH_SEP + "keywords" + FILE_PATH_SEP + "keywords_v2_01.txt";
+        String keywordFilePathSecondHalf = "src" + FILE_PATH_SEP + "main" + FILE_PATH_SEP + "resources" + FILE_PATH_SEP + "keywords" + FILE_PATH_SEP + "keywords_v2_02.txt";
+        List<String> firstHalfKeywords = Util.readKeywords(keywordFilePathFirstHalf);
+        List<String> secHalfKeywords = Util.readKeywords(keywordFilePathSecondHalf);
+
+        logger.info("Found {} keywords in '{}' and {}", firstHalfKeywords.size() + secHalfKeywords.size(), keywordFilePathFirstHalf, keywordFilePathSecondHalf);
+
+        BaseQuery keywordQueryFirstHalf = new BaseQuery();
+        keywordQueryFirstHalf.setKeywordList(firstHalfKeywords);
+        keywordQueryFirstHalf.setBooleanOperatorType(BooleanOperatorType.OR);
+
+        BaseQuery keywordQuerySecondHalf = new BaseQuery();
+        keywordQuerySecondHalf.setKeywordList(secHalfKeywords);
+        keywordQuerySecondHalf.setBooleanOperatorType(BooleanOperatorType.OR);
+
+        BaseQuery softwareQuery = new BaseQuery();
+        softwareQuery.addKeyword("\"software\"");
+        softwareQuery.addKeyword("\"system\"");
+        softwareQuery.setBooleanOperatorType(BooleanOperatorType.OR);
+
+        BaseQuery securityQuery = new BaseQuery();
+        securityQuery.addKeyword("\"security\"");
+        securityQuery.setBooleanOperatorType(BooleanOperatorType.OR);
+
+
+        SearchTool searchtool = new SearchTool();
+        searchtool.addMetaDataFields(MetaDataType.TITLE, MetaDataType.ABSTRACT);
+        searchtool.addBaseQueries(keywordQueryFirstHalf, softwareQuery, securityQuery);
+        searchtool.setBooleanOperatorType(BooleanOperatorType.AND);
+
+        String ieeeSearchStringFirstHalf = searchtool.generateSearchQuery(DatabaseType.IEEE_XPLORE);
+        logger.info("Created search String for IEEE 1: {}", ieeeSearchStringFirstHalf);
+
+        searchtool.reset();
+        searchtool.addMetaDataFields(MetaDataType.TITLE, MetaDataType.ABSTRACT);
+        searchtool.addBaseQueries(keywordQuerySecondHalf, softwareQuery, securityQuery);
+        searchtool.setBooleanOperatorType(BooleanOperatorType.AND);
+
+        String ieeeSearchStringSecondHalf = searchtool.generateSearchQuery(DatabaseType.IEEE_XPLORE);
+        logger.info("Created search String for IEEE 2: {}", ieeeSearchStringSecondHalf);
+    }
+
+    private static void searchIEEE_TWO() {
+
+        String keywordFilePathFirstHalf = "src" + FILE_PATH_SEP + "main" + FILE_PATH_SEP + "resources" + FILE_PATH_SEP + "keywords" + FILE_PATH_SEP + "keywords_v2_01.txt";
+        String keywordFilePathSecondHalf = "src" + FILE_PATH_SEP + "main" + FILE_PATH_SEP + "resources" + FILE_PATH_SEP + "keywords" + FILE_PATH_SEP + "keywords_v2_02.txt";
+        List<String> firstHalfKeywords = Util.readKeywords(keywordFilePathFirstHalf);
+        List<String> secHalfKeywords = Util.readKeywords(keywordFilePathSecondHalf);
+
+        logger.info("Found {} keywords in '{}' and {}", firstHalfKeywords.size() + secHalfKeywords.size(), keywordFilePathFirstHalf, keywordFilePathSecondHalf);
+
+        BaseQuery keywordQueryFirstHalf = new BaseQuery();
+        keywordQueryFirstHalf.setKeywordList(firstHalfKeywords);
+        keywordQueryFirstHalf.setBooleanOperatorType(BooleanOperatorType.OR);
+
+        BaseQuery keywordQuerySecondHalf = new BaseQuery();
+        keywordQuerySecondHalf.setKeywordList(secHalfKeywords);
+        keywordQuerySecondHalf.setBooleanOperatorType(BooleanOperatorType.OR);
+
+        BaseQuery softwareQuery = new BaseQuery();
+        softwareQuery.addKeyword("\"software\"");
+        softwareQuery.setBooleanOperatorType(BooleanOperatorType.OR);
+
+        BaseQuery securityQuery = new BaseQuery();
+        securityQuery.addKeyword("\"security\"");
+        securityQuery.setBooleanOperatorType(BooleanOperatorType.OR);
+
+
+        SearchTool searchtool = new SearchTool();
+        searchtool.addMetaDataFields(MetaDataType.TITLE, MetaDataType.ABSTRACT, MetaDataType.KEYWORDS);
+        searchtool.addBaseQueries(keywordQueryFirstHalf, softwareQuery, securityQuery);
+        searchtool.setBooleanOperatorType(BooleanOperatorType.AND);
+
+        String ieeeSearchStringFirstHalf = searchtool.generateSearchQuery(DatabaseType.IEEE_XPLORE);
+        logger.info("Created search String for IEEE 1: {}", ieeeSearchStringFirstHalf);
+
+        searchtool.reset();
+        searchtool.addMetaDataFields(MetaDataType.TITLE, MetaDataType.ABSTRACT, MetaDataType.KEYWORDS);
+        searchtool.addBaseQueries(keywordQuerySecondHalf, softwareQuery, securityQuery);
+        searchtool.setBooleanOperatorType(BooleanOperatorType.AND);
+
+        String ieeeSearchStringSecondHalf = searchtool.generateSearchQuery(DatabaseType.IEEE_XPLORE);
+        logger.info("Created search String for IEEE 2: {}", ieeeSearchStringSecondHalf);
+    }
+
+    private static void searchIEEE_ONE() {
+
+        String keywordFilePathFirstHalf = "src" + FILE_PATH_SEP + "main" + FILE_PATH_SEP + "resources" + FILE_PATH_SEP + "keywords" + FILE_PATH_SEP + "keywords_v2_01.txt";
+        String keywordFilePathSecondHalf = "src" + FILE_PATH_SEP + "main" + FILE_PATH_SEP + "resources" + FILE_PATH_SEP + "keywords" + FILE_PATH_SEP + "keywords_v2_02.txt";
+        List<String> firstHalfKeywords = Util.readKeywords(keywordFilePathFirstHalf);
+        List<String> secHalfKeywords = Util.readKeywords(keywordFilePathSecondHalf);
+
+        logger.info("Found {} keywords in '{}' and {}", firstHalfKeywords.size() + secHalfKeywords.size(), keywordFilePathFirstHalf, keywordFilePathSecondHalf);
+
+        BaseQuery keywordQueryFirstHalf = new BaseQuery();
+        keywordQueryFirstHalf.setKeywordList(firstHalfKeywords);
+        keywordQueryFirstHalf.setBooleanOperatorType(BooleanOperatorType.OR);
+
+        BaseQuery keywordQuerySecondHalf = new BaseQuery();
+        keywordQuerySecondHalf.setKeywordList(secHalfKeywords);
+        keywordQuerySecondHalf.setBooleanOperatorType(BooleanOperatorType.OR);
+
+        BaseQuery softwareQuery = new BaseQuery();
+        softwareQuery.addKeyword("\"software\"");
+        softwareQuery.setBooleanOperatorType(BooleanOperatorType.OR);
+
+        BaseQuery securityQuery = new BaseQuery();
+        securityQuery.addKeyword("\"security\"");
+        securityQuery.setBooleanOperatorType(BooleanOperatorType.OR);
+
+
+        SearchTool searchtool = new SearchTool();
+        searchtool.addMetaDataFields(MetaDataType.TITLE, MetaDataType.ABSTRACT);
+        searchtool.addBaseQueries(keywordQueryFirstHalf, softwareQuery, securityQuery);
+        searchtool.setBooleanOperatorType(BooleanOperatorType.AND);
+
+        String ieeeSearchStringFirstHalf = searchtool.generateSearchQuery(DatabaseType.IEEE_XPLORE);
+        logger.info("Created search String for IEEE 1: {}", ieeeSearchStringFirstHalf);
+
+        searchtool.reset();
+        searchtool.addMetaDataFields(MetaDataType.TITLE, MetaDataType.ABSTRACT);
+        searchtool.addBaseQueries(keywordQuerySecondHalf, softwareQuery, securityQuery);
+        searchtool.setBooleanOperatorType(BooleanOperatorType.AND);
+
+        String ieeeSearchStringSecondHalf = searchtool.generateSearchQuery(DatabaseType.IEEE_XPLORE);
+        logger.info("Created search String for IEEE 2: {}", ieeeSearchStringSecondHalf);
+    }
+
+}
--- src/main/java/at/jku/scsl/main/Main.java
+++ src/main/java/at/jku/scsl/main/Main.java
@@ -1,66 +1,20 @@
 package at.jku.scsl.main;
 
-import at.jku.scsl.searchtool.BaseQuery;
-import at.jku.scsl.searchtool.SearchTool;
-import at.jku.scsl.searchtool.types.BooleanOperatorType;
-import at.jku.scsl.searchtool.types.DatabaseType;
-import at.jku.scsl.searchtool.types.MetaDataType;
-import at.jku.scsl.util.Util;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import java.util.List;
-
-import static at.jku.scsl.util.Util.FILE_PATH_SEP;
+import static at.jku.scsl.main.ACMSearch.searchACM;
 
 
 public class Main {
     private static final Logger logger = LogManager.getLogger(Main.class);
 
     public static void main(String[] args) {
-
         logger.info("Welcome to the literature search tool!");
 
-        String keywordFilePath = "src" + FILE_PATH_SEP + "main" + FILE_PATH_SEP + "resources" + FILE_PATH_SEP + "keywords" + FILE_PATH_SEP + "keywords_v2.txt";
-        List<String> allKeywords = Util.readKeywords(keywordFilePath);
+//        searchIEEE();
+        searchACM();
 
-        logger.info("Found {} keywords in '{}'", allKeywords.size(), keywordFilePath);
-
-        List<String> firstHalfKeywords = allKeywords.subList(0, allKeywords.size() / 2);
-        List<String> secHalfKeywords = allKeywords.subList(allKeywords.size() / 2, allKeywords.size());
-
-        BaseQuery keywordQueryFirstHalf = new BaseQuery();
-        keywordQueryFirstHalf.setKeywordList(firstHalfKeywords);
-        keywordQueryFirstHalf.setBooleanOperatorType(BooleanOperatorType.OR);
-
-        BaseQuery keywordQuerySecondHalf = new BaseQuery();
-        keywordQuerySecondHalf.setKeywordList(secHalfKeywords);
-        keywordQuerySecondHalf.setBooleanOperatorType(BooleanOperatorType.OR);
-
-        BaseQuery softwareQuery = new BaseQuery();
-        softwareQuery.addKeyword("\"software\"");
-        softwareQuery.setBooleanOperatorType(BooleanOperatorType.OR);
-
-        BaseQuery securityQuery = new BaseQuery();
-        securityQuery.addKeyword("\"security\"");
-        securityQuery.setBooleanOperatorType(BooleanOperatorType.OR);
-
-
-        SearchTool searchtool = new SearchTool();
-        searchtool.addMetaDataFields(MetaDataType.TITLE, MetaDataType.ABSTRACT);
-        searchtool.addBaseQueries(keywordQueryFirstHalf, softwareQuery, securityQuery);
-        searchtool.setBooleanOperatorType(BooleanOperatorType.AND);
-
-        String ieeeSearchStringFirstHalf = searchtool.generateSearchQuery(DatabaseType.IEEE_XPLORE);
-        logger.info("Created search String for IEEE 1: {}", ieeeSearchStringFirstHalf);
-
-        searchtool.reset();
-        searchtool.addMetaDataFields(MetaDataType.TITLE, MetaDataType.ABSTRACT);
-        searchtool.addBaseQueries(keywordQuerySecondHalf, softwareQuery, securityQuery);
-        searchtool.setBooleanOperatorType(BooleanOperatorType.AND);
-
-        String ieeeSearchStringSecondHalf = searchtool.generateSearchQuery(DatabaseType.IEEE_XPLORE);
-        logger.info("Created search String for IEEE 2: {}", ieeeSearchStringSecondHalf);
     }
 
 
